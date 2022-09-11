f96fb87401428657cdb176b2415c23628bfdb732
danilowhk <danilowhk@gmail.com>
2022-09-11 05:15:53 -0300

    smartContract connection

--- pages/create-project.js
+++ pages/create-project.js
@@ -1,28 +1,67 @@
-import ethers from 'ethers'
+import {ethers} from 'ethers'
+import { useEffect, useState } from 'react'
+import Totem from './utils/Totem.json'
+
 export default function CreateProject() {
-  const handleCreateProject = (e) => {
+  const [account,setAccount] = useState()
+  const [totemContract,setTotemContract] = useState()
+  const [courseName,setCourseName] = useState()
+  const [ownerAddress,setOwnerAddress] = useState()
+  const [tokenAddress, setTokenAddress] = useState()
+  const [stakeAmount, setStakeAmount] = useState()
+  const totemContractAddress="0xc57B6B0efaf07b546c9AeE8AF8cA984660167258"
+  const totemContractAbi= Totem.abi
+
+  const getWallet = async () => {
+    if(window.ethereum){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      console.log("account",signer)
+      console.log('totemAbi',totemContractAbi)
+      console.log('Contract Address',totemContractAddress )
+      setAccount(signer)
+      const totemContractTemp = new ethers.Contract(totemContractAddress,totemContractAbi,signer)
+      console.log("Contract",totemContractTemp )
+      setTotemContract(totemContractTemp);
+
+
+    }
+  }
+
+  useEffect(()=>{
+    getWallet()
+  },[])
+
+  const handleCreateProject = async (e) => {
     e.preventDefault()
 
+   
+
+    console.log(totemContract)
+    const tx = await totemContract.addCourse( courseName, ownerAddress, stakeAmount, tokenAddress)
+    console.log(tx)
   }
+
+
 
     return (
       <form className="space-y-8 divide-y divide-gray-200 p-10">
         <div className="space-y-8 divide-y divide-gray-200">
           <div>
             <div>
-              <h3 className="text-lg font-medium leading-6 text-gray-900">Profile</h3>
-              <p className="mt-1 text-sm text-gray-500">
-                This information will be displayed publicly so be careful what you share.
+              <h3 className="text-2xl font-medium leading-6 text-gray-900 text-center">Crie o seu curso</h3>
+              <p className="mt-3 text-sm text-gray-500 text-lg text-center">
+                Crie um curso e defina a token de remunerção para os seus alunos
               </p>
             </div>
   
-            <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+            <div className="mt-1 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
               <div className="sm:col-span-4">
               </div>
   
             
- 
- 
+
             </div>
           </div>
   
@@ -37,7 +76,7 @@ export default function CreateProject() {
                     id="about"
                     name="about"
                     rows={3}
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    className="block w-full rounded-md border-gray-400 border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     defaultValue={''}
                   />
                 </div>
@@ -46,85 +85,57 @@ export default function CreateProject() {
             <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
               <div className="sm:col-span-3">
                 <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                  Endereço do Token
+                  Nome
                 </label>
                 <div className="mt-1">
                   <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    onChange={(e) => setCourseName(e.target.value)}
+                    className="block w-full rounded-md border-gray-400 border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
               </div>
   
               <div className="sm:col-span-3">
                 <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                  Valor
+                  Endereço do Instrutor
                 </label>
                 <div className="mt-1">
                   <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    onChange={(e) => setOwnerAddress(e.target.value)}
+                    className="block w-full rounded-md border-gray-400 border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+  
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                  Endereço do Token
+                </label>
+                <div className="mt-1">
+                  <input
+                    onChange={(e) => setTokenAddress(e.target.value)}
+                    className="block w-full rounded-md border-gray-400 border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+  
+              <div className="sm:col-span-3">
+                <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                  Valor de Stake
+                </label>
+                <div className="mt-1">
+                  <input
+                    onChange={(e) => setStakeAmount(e.target.value)}
+                    className="block w-full rounded-md border-gray-400 border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
               </div>
   
             </div>
           </div>
-  
-          <div className="pt-8">
-            <div>
-              <h3 className="text-lg font-medium leading-6 text-gray-900">Notifications</h3>
-              <p className="mt-1 text-sm text-gray-500">
-                We'll always let you know about important changes, but you pick what else you want to hear about.
-              </p>
-            </div>
-            <div className="mt-6">
-  
-              <fieldset className="mt-6">
-                <div className="mt-4 space-y-4">
-                  <div className="flex items-center">
-                    <input
-                      id="push-everything"
-                      name="push-notifications"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                    />
-                    <label htmlFor="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
-                      Everything
-                    </label>
-                  </div>
-                  <div className="flex items-center">
-                    <input
-                      id="push-email"
-                      name="push-notifications"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                    />
-                    <label htmlFor="push-email" className="ml-3 block text-sm font-medium text-gray-700">
-                      Same as email
-                    </label>
-                  </div>
-                  <div className="flex items-center">
-                    <input
-                      id="push-nothing"
-                      name="push-notifications"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                    />
-                    <label htmlFor="push-nothing" className="ml-3 block text-sm font-medium text-gray-700">
-                      No push notifications
-                    </label>
-                  </div>
-                </div>
-              </fieldset>
-            </div>
-          </div>
+
         </div>
   
         <div className="pt-5">
